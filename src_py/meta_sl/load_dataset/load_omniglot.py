fb5dae87211d336facb63fcbcb4797f4e906a415
Soonkyu Jeong <reingel@gmail.com>
2023-09-17 12:34:31 +0900

    Added src_py folder and load_omniglot.py file.

---
+++ src_py/meta_sl/load_dataset/load_omniglot.py
@@ -0,0 +1,8 @@
+import warnings
+from typing import Any, Dict, Tuple
+
+import matplotlib.pyplot as plt
+from torchmeta.datasets.helpers import omniglot
+from torchmeta.utils.data import BatchMetaDataLoader
+
+warnings.filterwarnings(action="ignore")
